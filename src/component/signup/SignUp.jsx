a04ff441e76a77bec96d14286a8179340f5f2ddb
anish-singh-rawat <anishsinghrawat1>
2023-11-18 17:02:31 +0530

    some changes in code

--- src/component/signup/SignUp.jsx
+++ src/component/signup/SignUp.jsx
@@ -5,88 +5,91 @@ import './SignUp.css'
 import { useFormik } from 'formik'
 import { schema } from '../../utilities/utilities'
 import { signup } from '../../Redux/slice/signUp'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
   const navigate = useNavigate()
 
-  const formik = useFormik({
+  const formikData = useFormik({
     initialValues: {
       username: '',
       userpassword: '',
       role: 'guest'
     },
     validationSchema: schema,
-    onSubmit: (values, actions) => {
-      try {
-        dispatch(signup(values)); 
+    onSubmit: (values) => {
+      if (values.username.length < 5) {
+        toast.warning("user name must be atleast 5 charecter!");
+      }
+      else if (values.userpassword.length < 6) {
+        toast.warning("user password must be atleast 6 charecter")
+      }
+      else {
+        dispatch(signup(values));
         navigate("/login")
-      } catch (error) { }
+      }
     },
   })
 
   return (
-    <div className="container-fluid containe-for-sub-box">
-      <div className="row">
-        <div className="col">
-          <div className="parent-form-div-sign-up">
-            <div className="sub-form-data">
-              <div className="form-top-heading mt-2">Sign Up</div>
-              <form className='mt-2' onSubmit={formik.handleSubmit}>
+    <>
+      <ToastContainer />
+      <div className="container-fluid containe-for-sub-box">
+        <div className="row">
+          <div className="col">
+            <div className="parent-form-div-sign-up">
+              <div className="sub-form-data">
+                <div className="form-top-heading mt-2">Sign Up</div>
+                <form className='mt-2' onSubmit={formikData.handleSubmit}>
 
-                <div className="user-name-feild-div">
-                  <label htmlFor="" className='user-name-label-feild'> UserName </label>
-                  <br />
-                  <input type="text" name='username'
-                    onChange={formik.handleChange}
-                    value={formik.values.username} className='user-name-input mt-2' />
+                  <div className="user-name-feild-div">
+                    <label htmlFor="" className='user-name-label-feild'> UserName </label>
+                    <br />
+                    <input type="text" name='username'
+                      onChange={formikData.handleChange}
+                      value={formikData.values.username} className='user-name-input mt-2' />
+                    {formikData.errors.username && formikData.touched.username &&
+                      <p className="text-danger">{formikData.errors.username}</p>}
 
-                  {
-                    formik.errors.username &&
-                    <p style={{ color: 'red' }}>
-                      {formik.errors.username}
-                    </p>
-                  }
+                  </div>
 
-                </div>
+                  <div className="user-password-feild mt-5">
+                    <label htmlFor="" className='user-password-label-feild'> Password </label>
+                    <br />
+                    <input type="password" name='userpassword'
+                      onChange={formikData.handleChange}
+                      value={formikData.values.userpassword} className='user-password-input mt-2' />
+                    {formikData.errors.userpassword && formikData.touched.userpassword &&
+                      <p className="text-danger">{formikData.errors.userpassword}</p>}
+                  </div>
 
-                <div className="user-password-feild mt-5">
-                  <label htmlFor="" className='user-password-label-feild'> Password </label>
-                  <br />
-                  <input type="password" name='userpassword'
-                    onChange={formik.handleChange}
-                    value={formik.values.userpassword} className='user-password-input mt-2' />
-                  {
-                    formik.errors.userpassword &&
-                    <p style={{ color: 'red' }}>
-                      {formik.errors.userpassword}
-                    </p>
-                  }
-                </div>
+                  <div className="sign-up-role-div mt-5">
+                    <div className="role-heading">Role</div>
 
-                <div className="sign-up-role-div mt-5">
-                  <div className="role-heading">Role</div>
+                    <select name='role' onBlur={formikData.handleBlur}
+                      onChange={formikData.handleChange}
+                      className='select-role-options mt-3'>
+                      <option value="guest">Guest</option>
+                      <option value="admin">Admin</option>
 
-                  <select name='role' onBlur={formik.handleBlur}
-                    onChange={formik.handleChange}
-                    className='select-role-options mt-3'>
-                    <option value="guest">Guest</option>
-                    <option value="admin">Admin</option>
+                    </select>
+                  </div>
 
-                  </select>
-                </div>
+                  <div className="button-parent mt-5 d-flex justify-content-around p-3">
+                    <button to={'/signup'} type='submit'
+                      className="singup-btn btn btn-success">Sign Up</button>
+                    <Link to={'/login'} className="login-btn btn btn-success">Sign in</Link>
+                  </div>
 
-                <div className="button-parent mt-5 d-flex justify-content-around">
-                  <button to={'/signup'} type='submit'
-                    className="singup-btn btn btn-success">Sign Up</button>
-                  <Link to={'/login'} className="login-btn btn btn-success">Sign in</Link>
-                </div>
-
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+
   )
 }
 
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,9 +1,6 @@
 import * as yup from 'yup'
 
 export const schema = yup.object().shape({
-    username: yup.string()
-      .required('this is required field'),
-
-    userpassword: yup.string()
-      .required('this is required field'),
-  })
+  username     : yup.string().trim().min(4).required("*UserName is Required"),
+  userpassword : yup.string().trim().min(5).required("*Password is Required"),
+});
